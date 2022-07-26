579e53b7b3dc5b56d75bbc9382683ea141cfa649
Muhammad Abdullah <mafivs51@gmail.com>
2022-07-26 06:02:14 +0500

    title update

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ function App() {
   return (
     //using jsx fragment because it returns only 1 tag
     <>
-<<<<<<< HEAD
       <MyNavbar title="Text Editor" menu1="Home" menu2="About Us" />
-=======
-      <MyNavbar title="My Project" menu1="Home" menu2="About Us" />
->>>>>>> ad502aeb1d12985f14845044954fc05efa32126e
     </>
   );
 }
